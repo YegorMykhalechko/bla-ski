a7d7ae303efd07cf5d781312fd51e918c0378dcd
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-10-09 19:39:08 +0200

    create event component

---
+++ src/components/Events/EventCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="col-12 md:col-5 lg:col-3 p-3">
+    <Card>
+      <template #header>
+        <img
+          class="card-img"
+          src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png"
+        />
+      </template>
+      <template #title>Resort</template>
+      <template #subtitle>
+        <div>Date Time</div>
+        <div>Price</div>
+        <div>Slots(1/4)</div>
+      </template>
+      <template #content>
+        <p>Description</p>
+      </template>
+      <template #footer>
+        <Button icon="pi pi-check" label="Book" />
+      </template>
+    </Card>
+  </div>
+</template>
+
+<style scoped>
+.card-img {
+  height: 10rem;
+  object-fit: contain;
+}
+</style>
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,36 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from "../components/Events/EventCard.vue";</script>
 
 <template>
   <div class="grid grid-nogutter">
-    <h1>Events</h1>
-    <div class="col-12 p-3">
-      <Card>
-        <template #header>
-          <img
-            class="card-img"
-            src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png"
-          />
-        </template>
-        <template #title> Advanced Card </template>
-        <template #subtitle> Card subtitle </template>
-        <template #content>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-            sed consequuntur error repudiandae numquam deserunt quisquam
-            repellat libero asperiores earum nam nobis, culpa ratione quam
-            perferendis esse, cupiditate neque quas!
-          </p>
-        </template>
-        <template #footer>
-          <Button icon="pi pi-check" label="Save" />
-          <Button
-            icon="pi pi-times"
-            label="Cancel"
-            class="p-button-secondary"
-          />
-        </template>
-      </Card>
-    </div>
+    <EventCard />
   </div>
 </template>
 
