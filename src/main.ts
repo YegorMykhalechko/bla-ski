71e47eec006afbbb8a8e42d81379ab2dbc99b1e1
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-10-09 18:11:22 +0200

    added primeflex

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,15 @@ import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
 
-import Menubar from 'primevue/menubar';
-import Button from 'primevue/button';
+import Menubar from "primevue/menubar";
+import Button from "primevue/button";
+import Card from "primevue/card";
 
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
-import "./assets/main.css"
+import "/node_modules/primeflex/primeflex.css";
+import "./assets/main.css";
 
 const app = createApp(App);
 
@@ -19,7 +21,8 @@ app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
 
-app.component('Menubar', Menubar);
-app.component('Button', Button);
+app.component("Menubar", Menubar);
+app.component("Button", Button);
+app.component("Card", Card);
 
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-import { useCounterStore } from "@/stores/counter";
-const store = useCounterStore();
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Home</h1>
-  {{store.count}}
 </template>
 
 <style scoped></style>
